80d90988928193e58e5b6f0986ebfae2ff5b9085
leandroh1002 <leandroherrera1002@gmail.com>
2025-05-09 19:55:44 -0300

    add categories and option to swagger

--- src/routes/categories.route.js
+++ src/routes/categories.route.js
@@ -10,68 +10,123 @@ const deleteCategoriesOptionsController = require("../controllers/categories_opt
 const categoriesRouter = Router();
 
 categoriesRouter.get("/categories", getCategories);
-// /**
-//  * @swagger
-//  * /categories:
-//  *   get:
-//  *     summary: Obtener todas las categorías.
-//  *     tags:
-//  *       - Categories
-//  *     responses:
-//  *       200:
-//  *         description: > 
-//  *           Obtiene un objeto con dos propiedades: count y data.
-//  *           count: contiene la cantidad de categorías registradas.
-//  *           data: contiene un array de objetos, cada objeto contiene una categoría y sus propiedades.  
-//  *       404:
-//  *         description: No hay registro de categorías.
-//  *       500:
-//  *         description: Error interno del servidor.
-//  */
+/**
+ * @swagger
+ * /categories:
+ *   get:
+ *     summary: Obtener todas las categorías.
+ *     tags:
+ *       - Categories
+ *     responses:
+ *       200:
+ *         description: > 
+ *           Obtiene un objeto con dos propiedades: count y data.
+ *           count: contiene la cantidad de categorías registradas.
+ *           data: contiene un array de objetos, cada objeto contiene una categoría y sus propiedades.  
+ *       404:
+ *         description: No hay registro de categorías.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 
 categoriesRouter.get("/categories/options", getCategoriesOptions);
-// /**
-//  * @swagger
-//  * /categories/options:
-//  *   get:
-//  *     summary: Obtener opciones de categorías.
-//  *     tags:
-//  *       - Categories
-//  *     responses:
-//  *       200:
-//  *         description: > 
-//  *           Obtiene un array de objetos, cada objeto contiene una opción de categoría y sus propiedades.
-//  *       404:
-//  *         description: No hay datos en categories_options.
-//  *       500:
-//  *         description: Error interno del servidor.
-//  */
+/**
+ * @swagger
+ * /categories/options:
+ *   get:
+ *     summary: Obtener todas las opciones.
+ *     tags:
+ *       - Categories
+ *     responses:
+ *       200:
+ *         description: > 
+ *           Obtiene un array de objetos, cada objeto contiene una opción de categoría y sus propiedades.
+ *       404:
+ *         description: No hay datos en categories_options.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 
 categoriesRouter.post("/categories", postCategories);
-// /**
-//  * @swagger
-//  * /categories:
-//  *   post:
-//  *     summary: Crear una nueva categoría.
-//  *     tags:
-//  *       - Categories
-//  *     requestBody:
-//  *       required: true
-//  *       content:
-//  *         application/json:
-//  *           schema:
-//  *             $ref: '#/components/schemas/CategoryInput'
-//  *     responses:
-//  *       201:
-//  *         description: La categoría ha sido creada exitosamente.
-//  *       400:
-//  *         description: Petición incorrecta, los datos enviados no son válidos.
-//  *       500:
-//  *         description: Error interno del servidor.
-//  */
+/**
+ * @swagger
+ * /categories:
+ *   post:
+ *     summary: Obtener todas las Caetgorias.
+ *     tags:
+ *       - Categories
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               description:
+ *                 type: string
+ *                 description: Nombre de la categoria.
+ *     responses:
+ *       201:
+ *         description: La categoría ha sido creada exitosamente.
+ *       400:
+ *         description: Petición incorrecta, los datos enviados no son válidos.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 
 categoriesRouter.post("/categories/options", postCategoriesOptionsController);
-
+/**
+ * @swagger
+ * /categories/options:
+ *   post:
+ *     summary: Crear una nueva opcion.
+ *     tags:
+ *       - Categories
+ *     requestBody:
+ *       required: true
+ *       content:
+ *        application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               idCategorie:
+ *                 type: string
+ *                 description: El id de la categoria a la que vayas asignar.
+ *               description:
+ *                 type: string
+ *                 description: Nombre de la opcion.
+ *     responses:
+ *       201:
+ *         description: La categoría ha sido creada exitosamente.
+ *       400:
+ *         description: Petición incorrecta, los datos enviados no son válidos.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 categoriesRouter.delete("/categories/options", deleteCategoriesOptionsController);
-
+/**
+ * @swagger
+ * /categories/options:
+ *   delete:
+ *     summary: Eliminar alguna opcion.
+ *     tags:
+ *       - Categories
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/x-www-form-urlencoded:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               idOption:
+ *                 type: string
+ *                 description: ID de la opcion.
+ *     responses:
+ *       201:
+ *         description: La categoría ha sido eliminada exitosamente.
+ *       400:
+ *         description: Petición incorrecta, los datos enviados no son válidos.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 module.exports = categoriesRouter;
